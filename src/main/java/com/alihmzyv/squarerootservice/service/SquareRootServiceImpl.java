ec6331bc45eed663bcff8ae90640ba52db7676a1
alihmzyv <alihmzyv@gmail.com>
2023-04-26 18:44:44 +0400

    build server manually

---
+++ src/main/java/com/alihmzyv/squarerootservice/service/SquareRootServiceImpl.java
@@ -0,0 +1,35 @@
+package com.alihmzyv.squarerootservice.service;
+
+import com.proto.squareroot.SquareRootRequest;
+import com.proto.squareroot.SquareRootResponse;
+import com.proto.squareroot.SquareRootServiceGrpc;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import io.grpc.stub.annotations.GrpcGenerated;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class SquareRootServiceImpl extends SquareRootServiceGrpc.SquareRootServiceImplBase {
+    @Override
+    public void squareRoot(SquareRootRequest request, StreamObserver<SquareRootResponse> responseObserver) {
+        if (!request.hasInputValue()) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription("Input value should be set")
+                    .asRuntimeException());
+        } else {
+            double inputValue = request.getInputValue();
+            if (inputValue < 0) {
+                responseObserver.onError(Status.INVALID_ARGUMENT
+                        .withDescription("Input value should be equal to or greater than 0")
+                        .asRuntimeException());
+            } else {
+                SquareRootResponse squareRootResponse = SquareRootResponse.newBuilder()
+                        .setResult(Math.sqrt(inputValue))
+                        .build();
+                responseObserver.onNext(squareRootResponse);
+                responseObserver.onCompleted();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/alihmzyv/squarerootservice/SquareRootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alihmzyv.squarerootservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareRootServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
